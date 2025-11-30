37d1e19885992202a39a4b4595a4de895c6a9647
AnkitKumarDelta <ankitna355@gmail.com>
2025-11-30 21:35:19 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,44 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[items, setItems] = useState([{name:"",age:"",}]);
+
+  function handleAdd(){
+    setItems([...items, {name:"",age:"",}]);
+  }
+
+  function handleChange(index,e){
+    let new_items = [...items];
+    new_items[index][e.target.name] = e.target.value;
+    setItems(new_items);
+  }
+  function handleRemove(index){
+    let new_items = items.filter((i,ind)=>ind!==index);
+    setItems(new_items);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <form onSubmit={(e)=>{
+          e.preventDefault();
+          console.log(items);
+        }}>
+          {
+            items.map((i,ind)=>{
+              return(
+                <div>
+            <input type="text" value={i.name} name="name" placeholder="Name" onChange={(e)=>handleChange(ind,e)}/>
+            <input type="number" value={i.age} name="age" placeholder="Age" onChange={(e)=>handleChange(ind,e)}/>
+            <button onClick={()=>handleRemove(ind)}>Remove</button>
+          </div>
+              )
+            })
+          }
+
+          <button>Submit</button>
+          <button onClick={handleAdd}>Add More..</button>
+        </form>
     </div>
   )
 }
